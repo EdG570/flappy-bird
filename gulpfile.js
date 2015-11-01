df91b165832a4e1c09fe3f67c6d57c6c65d3bbb5
Ed Glazenski <edglazenski@gmail.com>
2015-11-01 15:16:06 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+// JavaScript linting task
+gulp.task('jshint', function() {
+  return gulp.src('site/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Compile Sass task
+gulp.task('sass', function() {
+  return gulp.src('site/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('site/css'));
+});
+
+// Watch task
+gulp.task('watch', function() {
+  gulp.watch('site/js/*.js', ['jshint']);
+  gulp.watch('site/scss/styles.scss', ['sass']);
+});
+
+// Minify index
+gulp.task('html', function() {
+  return gulp.src('site/index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('build/'));
+});
+
+// JavaScript build task, removes whitespace and concatenates all files
+gulp.task('scripts', function() {
+  return browserify('site/js/main.js')
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+// Styles build task, concatenates all the files
+gulp.task('styles', function() {
+  return gulp.src('site/css/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('build/css'));
+});
+
+// Image optimization task
+gulp.task('images', function() {
+  return gulp.src('site/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/images'));
+});
+
+// Image optimization task
+gulp.task('css-images', function() {
+  return gulp.src('site/css/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/css/images'));
+});
+
+// Default task
+gulp.task('default', ['jshint', 'sass', 'watch']);
+
+// Build task
+gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images', 'css-images']);
---
+++ site/js/systems/graphics.js
@@ -0,0 +1,23 @@
+var GraphicsSystem = function(entitites) {
+  this.entities = entities;
+}
+
+GraphicsSystem.prototype.run = function() {
+  //Tick the graphics system to see it in action
+  for(var i = 0; i < 5; i++) {
+    this.tick();
+  }
+}
+
+GraphicsSystem.prototype.tick = function() {
+    for (var i=0; i < this.entities.length; i++) {
+      var entitity = this.entities[i];
+      if (!'graphics' in entity.components) {
+        continue;
+      }
+
+      entity.components.graphics.draw(this.context);
+    }
+};
+
+exports.GraphicsSystem = GraphicsSystem;
